cf8b688bfe12cb850ec33d6796151fda3cdaf080
Angelika Makarova <esipovaangelika@mail.ru>
2022-01-27 10:33:12 +0300

    Implement AppointmentsPage and AppointmentForm

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ContactPicker} from '../contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
@@ -18,8 +19,26 @@ export const AppointmentForm = ({
       .split("/");
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
+  const addTitle = ({target}) => {
+    setTitle(target.value);
+  }
+  const addDate = ({target}) => {
+    setDate(target.value);
+  }
+  const addTime = ({target}) => {
+    setTime(target.value);
+  }
+  const chooseContact = ({target}) => {
+    setContact(target.value);
+  }
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={title} onChange={addTitle} />
+      <input type='date' value={date} onChange={addDate} />
+      <input min={getTodayString()} type='time' value={time} onChange={addTime} />
+      <ContactPicker contacts={contacts} onChange={chooseContact} />
+      <input type='submit' value='Add'/>
+    </form>
   );
 };
